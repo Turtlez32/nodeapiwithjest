594902efb1697607c8784744f7609b144b7c19b1
Matthew <zyther44@gmail.com>
2020-08-24 08:34:24 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const PostRoutes = require("./routes/post");
 const CartRoutes = require("./routes/cart");
 const PaymentRoutes = require("./routes/payment");
+const AdminRoutes = require("./routes/admin");
 
 const post = require("./models/post");
 const mongoose = require("mongoose");
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use("/api/posts", PostRoutes);
 app.use("/api/cart", CartRoutes);
 app.use("/api/pay", PaymentRoutes);
+app.use("/api/admin", AdminRoutes);
 app.listen(3000);
 
 //console.log("Server Running");
